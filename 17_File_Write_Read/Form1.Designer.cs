6749462cf04741aac6f8a8c708a364c1622d0342
InSung-Na <lht98323@gmail.com>
2023-06-30 00:36:29 +0900

    File_Write_Read: StreamWriter, StreamReader

---
+++ 17_File_Write_Read/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _17_File_Write_Read
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(22, 12);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(432, 225);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(22, 258);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(119, 68);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "파일 쓰기";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(335, 258);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(119, 68);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "파일 읽기";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(178, 258);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(119, 68);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "내용 초기화";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 382);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.rtb);
+            this.Name = "Form1";
+            this.Text = "파일 입출력";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ 17_File_Write_Read/Form1.cs
@@ -0,0 +1,64 @@
+﻿// 파일 입출력
+/*
+ - StreamWriter("FILE"): FILE에 데이터 저장하는 클래스
+    - sw.WriteLine("TEXT"): 내용 저장
+    - @"SAVE_PATH": 파일 경로 설정 ex. @"D:\data.txt"
+
+ - StreamReader("FILE"): FILE의 데이터 읽는 클래스
+    - sr.ReadLine();: 내용 한줄 읽기
+    - while(!sr.EndOfStream){sr.ReadLine();}: 모든 내용 읽기
+ */
+
+using System;
+using System.IO;    // 입출력 네임스페이스 사용선언
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17_File_Write_Read
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            // 데이터 쓰기 클래스 선언
+            // .rtf: 워드 확장자
+            using (StreamWriter sw = new StreamWriter("data.rtf"))
+            {
+                sw.WriteLine("+++ 입력내용 +++");
+                sw.WriteLine(rtb.Text);
+                //sw.Close();
+            }
+            MessageBox.Show("저장되었습니다");
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // 데이터 읽기 클래스 선언
+            StreamReader sr = new StreamReader("data.rtf");
+            // stream이 마지막이 아니면 반복 실행
+            while(!sr.EndOfStream)
+            {
+                rtb.AppendText(sr.ReadLine() + Environment.NewLine);
+            }
+            MessageBox.Show("파일을 읽었습니다");
+            sr.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            rtb.Clear();
+            MessageBox.Show("출력 상자를 초기화했습니다");
+        }
+    }
+}
